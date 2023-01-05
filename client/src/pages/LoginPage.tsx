1e964dc3e2a3b5d122c88c217dc656f589bc9938
권유리 <99jiyusu@gmail.com>
2023-01-05 18:40:16 +0900

    feat: 이메일, 비밀번호 유효성 확인

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 const LoginPage = () => {
+  let [loginIsPossible, setLoginPossiblity] = useState(false); // ref
+  // 유효한 입력값인지 여부(빈 문자열이면 true?)
+  let [isValidId, setIsValidId] = useState<boolean>(true);
+  let [isValidPw, setIsValidPw] = useState<boolean>(true);
+  let idRef = useRef('');
+  let pwRef = useRef('');
+
+  const inputId = (e) => {
+    const inputValue = e.target.value;
+    idRef.current = inputValue;
+
+    // 유효한 ID 인지 검사
+    setIsValidId(inputValue.includes('@') && inputValue.includes('.') ? true : false);
+  };
+
+  const inputPw = (e) => {
+    const inputValue = e.target.value;
+    pwRef.current = inputValue;
+
+    // 입력되는 시점에서 빨간 메시지를 띄우기 위해 필요- > 추후 디바운싱으로 변경하기
+    setIsValidPw(inputValue.length >= 8 ? true : false);
+  };
+
+  useEffect(() => {
+    const isNotEmpty = idRef.current.length > 0 && pwRef.current.length > 0;
+    setLoginPossiblity(isValidId && isValidPw && isNotEmpty ? true : false);
+  }, [isValidId, isValidPw]);
+
   return (
     <LoginPageLayout>
       <LoginBox>
         <h1>로그인</h1>
         <label htmlFor="id">Id</label>
-        <input type="text" name="id" id="id" placeholder="아이디를 입력해주세요." />
+        <input type="text" name="id" id="id" placeholder="아이디를 입력해주세요." onChange={inputId} />
+        {!isValidId && idRef.current.length > 0 && <ErrorMsgParagraph>이메일 형식으로 입력해주세요.</ErrorMsgParagraph>}
         <label htmlFor="pw">Password</label>
-        <input type="password" name="pw" id="pw" placeholder="비밀번호를 입력해주세요." />
-        <LoginButton>로그인</LoginButton>
+        <input type="password" name="pw" id="pw" placeholder="비밀번호를 입력해주세요." onChange={inputPw} />
+        {!isValidPw && pwRef.current.length > 0 && <ErrorMsgParagraph>8자 이상 입력해주세요.</ErrorMsgParagraph>}
+        <LoginButton disabled={!loginIsPossible} onClick={() => console.log('로그인. 라우팅')}>
+          로그인
+        </LoginButton>
         <hr />
         <SignUpParagraph>
           아직 회원이 아니신가요?
@@ -95,6 +127,13 @@ export const LoginButton = styled.button`
   border-radius: 4px;
   background-color: #71ced9;
   color: white;
+
+  :disabled {
+    /* border: 1px solid #999999; */
+    background-color: #cccccc;
+    background-color: #828282;
+    /* color: #666666; */
+  }
 `;
 
 export const SignUpParagraph = styled.p`
@@ -114,4 +153,12 @@ export const SignUpParagraph = styled.p`
     background-color: white;
     color: #71ced9;
   }
+`;
+
+export const ErrorMsgParagraph = styled.p`
+  color: red;
+  font-size: 0.8rem;
+  text-align: left;
+  margin-top: 6px;
+  margin-left: 4px;
 `;
